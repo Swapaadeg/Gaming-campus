7cd4a02f2c5437374d48975bc0f740270924ebf0
Marie Rivier <marie.rivier23@gmail.com>
2025-04-02 16:35:20 +0200

    footer

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["src/**/*.{html,js}","*.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        roboto: ['roboto']
+      },
+      colors: {
+        purple1: '#853b8f'
+      }
+    },
+  },
+  plugins: [],
+}
+
